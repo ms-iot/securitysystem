8ebe001e01a59e037c9792be8529d540602d0c7d
daveraskin <draskal2@gmail.com>
2015-06-24 10:13:46 -0700

    switching branches

--- security_system_web_app/routes/index.js
+++ security_system_web_app/routes/index.js
@@ -1,42 +1,42 @@
-// var express = require('express');
-// var router = express.Router();
-// var env = require('node-env-file');
-// env('./.env')
-// var fs = require('fs');
-// var azure = require('azure-storage');
-// var bigInt = require('big-integer');
-// var blobService = azure.createBlobService();
+var express = require('express');
+var router = express.Router();
+var env = require('node-env-file');
+env('./.env')
+var fs = require('fs');
+var azure = require('azure-storage');
+var bigInt = require('big-integer');
+var blobService = azure.createBlobService();
 
 
-// router.get('/', function(req, res, next) {
-//   res.render('index')
-// });
+router.get('/', function(req, res, next) {
+  res.render('index')
+});
 
-// router.get('/images', function(req,res){
-//     var images = [];
-//         blobService.listBlobsSegmented('imagecontainer', null, function(error, result, response){
-//           if(!error){
-//             images = result;
-//             console.log(images.entries)
-//              for(var i = 0; i < images.entries.length; i++){
-//                 // using npm module bigInt, because the number of .NET ticks
-//                 // is a number with too many digits for vanilla JavaScript
-//                 // to perform accurate math on.
-//                 var ticks = images.entries[i].name.slice(0,18);
-//                 var ticksAtUnixEpoch = bigInt("621355968000000000")
-//                 var ticksInt = bigInt(ticks);
-//                 var ticksSinceUnixEpoch = ticksInt.minus(ticksAtUnixEpoch);
-//                 var milliseconds = ticksSinceUnixEpoch.divide(10000)
-//                 //Converting millisecond to dateTime client side so it will
-//                 //display in the user's local timezone.
-//                 images.entries[i].milliseconds = milliseconds.value
-//               }
-//             res.send(images);
-//           } else {
-//             res.send(error);
-//           }
-//         })
-// })
+router.get('/images', function(req,res){
+    var images = [];
+        blobService.listBlobsSegmented('imagecontainer', null, function(error, result, response){
+          if(!error){
+            images = result;
+            console.log(images.entries)
+             for(var i = 0; i < images.entries.length; i++){
+                // using npm module bigInt, because the number of .NET ticks
+                // is a number with too many digits for vanilla JavaScript
+                // to perform accurate math on.
+                var ticks = images.entries[i].name.slice(0,18);
+                var ticksAtUnixEpoch = bigInt("621355968000000000")
+                var ticksInt = bigInt(ticks);
+                var ticksSinceUnixEpoch = ticksInt.minus(ticksAtUnixEpoch);
+                var milliseconds = ticksSinceUnixEpoch.divide(10000)
+                //Converting millisecond to dateTime client side so it will
+                //display in the user's local timezone.
+                images.entries[i].milliseconds = milliseconds.value
+              }
+            res.send(images);
+          } else {
+            res.send(error);
+          }
+        })
+})
 
 
-// module.exports = router;
+module.exports = router;
