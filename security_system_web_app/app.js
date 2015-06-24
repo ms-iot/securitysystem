2d49b874e233c9ee81397b82b89e3b5bcff24a48
daveraskin <draskal2@gmail.com>
2015-06-24 10:18:40 -0700

    switching branches

--- security_system_web_app/app.js
+++ security_system_web_app/app.js
@@ -16,8 +16,8 @@ var fs = require('fs');
 var azure = require('azure-storage');
 var bigInt = require('big-integer');
 var blobService = azure.createBlobService();
-var auth = "./routes/auth";
-var index = "./routes/index";
+var auth = require("./routes/auth");
+var index = require("./routes/index");
 
 var app = express();
 
@@ -38,8 +38,8 @@ app.use(session({
   saveUninitialized: false}));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/auth', auth);
 app.use('/', index);
+app.use('/auth', auth);
 
 
 passport.serializeUser(function(user, done) {
@@ -54,75 +54,8 @@ passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
-app.get('/auth/github',
-  passport.authenticate('github', { scope: [ 'user:email' ] }));
 
-app.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-  console.log("redirection happening");
-    res.redirect('/');
-  });
 
-app.get('/auth/facebook',
-  passport.authenticate('facebook'));
-
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-  console.log("redirection happening");
-    res.redirect('/');
-  });
-
-app.get('/',
-  ensureAuthenticated,
-  function(req, res) {
-    console.log("opening home");
-    res.render('index');
-});
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    console.log('request authenticated');
-    return next();
-  }
-  console.log("not authenticated, going to login page");
-  res.redirect('/login');
-}
-
-app.get('/images',
-  ensureAuthenticated,
-  function(req,res){
-    var images = [];
-        blobService.listBlobsSegmented('imagecontainer', null, function(error, result, response){
-          if(!error){
-            images = result;
-            console.log(images.entries)
-             for(var i = 0; i < images.entries.length; i++){
-                // using npm module bigInt, because the number of .NET ticks
-                // is a number with too many digits for vanilla JavaScript
-                // to perform accurate math on.
-                var ticks = images.entries[i].name.slice(0,18);
-                var ticksAtUnixEpoch = bigInt("621355968000000000")
-                var ticksInt = bigInt(ticks);
-                var ticksSinceUnixEpoch = ticksInt.minus(ticksAtUnixEpoch);
-                var milliseconds = ticksSinceUnixEpoch.divide(10000)
-                //Converting millisecond to dateTime client side so it will
-                //display in the user's local timezone.
-                images.entries[i].milliseconds = milliseconds.value
-              }
-            res.send(images);
-          } else {
-            res.send(error);
-          }
-        })
-})
-
-app.get('/login', function(req, res){
-  res.render('login');
-});
 
 // app.use('/', routes);
 
