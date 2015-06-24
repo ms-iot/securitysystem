7ea04fe182bbe61ae54504f5b4ca28031ad8f642
daveraskin <draskal2@gmail.com>
2015-06-23 17:04:39 -0700

    switching branches

--- security_system_web_app/app.js
+++ security_system_web_app/app.js
@@ -24,8 +24,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
