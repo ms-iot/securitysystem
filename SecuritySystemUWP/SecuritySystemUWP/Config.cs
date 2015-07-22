34915bcc15b75920df9ebbd3d149c7fd14c6309f
Aparajita Dutta <apdutta@microsoft.com>
2015-07-22 14:50:03 -0700

    Migrating project to latest Windows10 tools

--- SecuritySystemUWP/SecuritySystemUWP/Config.cs
+++ SecuritySystemUWP/SecuritySystemUWP/Config.cs
@@ -8,15 +8,15 @@ namespace SecuritySystemUWP
 {
     public static class Config
     {
-        public static string StorageProvider = "Azure"; //This will be "Azure" or "OneDrive" based on the storage you are using for this project
+        public static string StorageProvider = ""; //This will be "Azure" or "OneDrive" based on the storage you are using for this project
 
         //If you are using Azure, update these values with your Azure Account Name and your Primary Access Key
-        public static string AzureAccountName = "securitysystemstorage"; 
-        public static string AzureAccessKey = "4M0uJQHmFIqSLPca7vO1aCjX6UGoCbgTbyKhEB88peLhqfceUVD6MgA8KqXd9euhnFmmpnbUbT3C+2LgByzsIA==";
+        public static string AzureAccountName = ""; 
+        public static string AzureAccessKey = "";
 
         //If you are using OneDrive, update these values with your Client ID and your Client Secret
-        public static string OneDriveClientId = "000000004015C31B";
-        public static string OneDriveClientSecret = "V7JaAb-9Kxd8vW3NQSzmE2ZM2aXWOlCk";
+        public static string OneDriveClientId = "";
+        public static string OneDriveClientSecret = "";
 
         //This value is the number of days for which your pictures will be stored
         public static int StorageDuration = 7;
