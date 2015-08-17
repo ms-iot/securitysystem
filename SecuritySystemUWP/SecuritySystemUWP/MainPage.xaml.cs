9cea677ddbfc1f3323ab79f9e5a423663813699d
jochi <jochi@microsoft.com>
2015-08-17 16:35:05 -0700

    Adding back MainPage button

--- SecuritySystemUWP/SecuritySystemUWP/MainPage.xaml.cs
+++ SecuritySystemUWP/SecuritySystemUWP/MainPage.xaml.cs
@@ -8,9 +8,6 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace SecuritySystemUWP
 {
     /// <summary>
@@ -18,9 +15,35 @@ namespace SecuritySystemUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private string[] cameras = { "Cam1" };
+        private static bool started = false;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void RunningToggle_Click(object sender, RoutedEventArgs e)
+        {
+            if (!started)
+            {
+                await App.Controller.Initialize();
+                started = true;
+                App.Controller.XmlSettings = await AppSettings.RestoreAsync("Settings.xml");
+                this.Frame.Navigate(App.Controller.Storage.StorageStartPage());
+            }
+            else
+            {
+                App.Controller.Dispose();
+                started = false;
+                this.Frame.Navigate(typeof(MainPage));
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            RunningToggle.Content = started ? "Stop" : "Start";
+            this.Frame.Navigate(App.Controller.Storage.StorageStartPage());
         }
     }
 }
