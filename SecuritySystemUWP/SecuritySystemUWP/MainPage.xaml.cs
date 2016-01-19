68ca301335a115f6530b17cac7ca7b06e6703a11
Masato Sudo <masatos@microsoft.com>
2016-01-19 13:57:59 -0800

    Bug fix to update UI dynamically when ip address and/or device name changes.

--- SecuritySystemUWP/SecuritySystemUWP/MainPage.xaml.cs
+++ SecuritySystemUWP/SecuritySystemUWP/MainPage.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using Windows.Networking.Connectivity;
+using Windows.UI.Core;
 
 namespace SecuritySystemUWP
 {
@@ -15,24 +17,59 @@ namespace SecuritySystemUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private CoreDispatcher _MainPageDispatcher = null;
+        
+
         public MainPage()
         {
-            string ipAddress = "";
-            string instructions = "";
-
             this.InitializeComponent();
 
-            // get device environment info and display it in UI
-            deviceNameValueTextBlock.Text = EnvironmentSettings.GetDeviceName();
-            ipAddress = EnvironmentSettings.GetIPAddress();
-            ipAddressValueTextBlock.Text = ipAddress;
+            _MainPageDispatcher = Window.Current.Dispatcher;
+
+            // network status change event
+            NetworkInformation.NetworkStatusChanged += NetworkInformation_NetworkStatusChanged;
+
+            // get static device environment info and display it in UI           
             appVersionValueTextBlock.Text = EnvironmentSettings.GetAppVersion();
             OSVersionValueTextBlock.Text = EnvironmentSettings.GetOSVersion();
 
-            // instructions text with ip address
-            instructions = "Setup Instructions: To configure this security system please go to URL http://" + ipAddress + ":8000 on a browser.";
-            instructionsTextBlock.Text = instructions;
+            UpdateNetworkInfo();
 
+        }
+
+        /// <summary>
+        /// On network status change update network info and display on main page
+        /// </summary>
+        /// <param name="sender"></param>
+        private async void NetworkInformation_NetworkStatusChanged(object sender)
+        {
+            await _MainPageDispatcher.RunAsync(CoreDispatcherPriority.Low, () =>
+            {
+                UpdateNetworkInfo();
+            });
+        }
+
+        /// <summary>
+        /// Get dynamic network information about the device and display it
+        /// </summary>
+        private void UpdateNetworkInfo()
+        {
+            string ipAddress = EnvironmentSettings.GetIPAddress();
+            string instructions = "";
+            // check if ip address is valid
+            if (ipAddress == "0.0.0.0")
+            {
+                ipAddress = "Invalid IP Address: 0.0.0.0";
+                instructions = "Setup Instructions: Please ensure your device has a valid ip address first.";
+            }
+            else {
+                instructions = "Setup Instructions: To configure this security system please go to URL http://" + ipAddress + ":8000 on a browser.";
+            }
+
+            // update UI
+            deviceNameValueTextBlock.Text = EnvironmentSettings.GetDeviceName();
+            ipAddressValueTextBlock.Text = ipAddress;
+            instructionsTextBlock.Text = instructions;
         }
     }
 }
